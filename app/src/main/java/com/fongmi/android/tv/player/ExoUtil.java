3e5427eacfb4e3c3bb5075ff72b66e4208c49b91
TangJiahui <tangjiahui0610@gmail.com>
2023-07-03 20:19:27 +0800

    exo add remote subtitle. fix keep and history getId lost subtitle files. merge from lastest dev

--- app/src/main/java/com/fongmi/android/tv/player/ExoUtil.java
+++ app/src/main/java/com/fongmi/android/tv/player/ExoUtil.java
@@ -123,7 +123,7 @@ public class ExoUtil {
 
     private static String getMimeType(String format, int errorCode) {
         if (format != null) return format;
-        if (errorCode == PlaybackException.ERROR_CODE_PARSING_MANIFEST_MALFORMED) return MimeTypes.APPLICATION_OCTET;
+        if (errorCode == PlaybackException.ERROR_CODE_PARSING_MANIFEST_MALFORMED) return MimeTypes.BASE_TYPE_APPLICATION + "/octet-stream";
         if (errorCode == PlaybackException.ERROR_CODE_PARSING_CONTAINER_UNSUPPORTED || errorCode == PlaybackException.ERROR_CODE_IO_UNSPECIFIED) return MimeTypes.APPLICATION_M3U8;
         return null;
     }
